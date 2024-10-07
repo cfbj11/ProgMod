3d5a3c4ac8b32bc7d6cffff341b0a9164262e5ac
cfbj11 <cfbriones@uamv.edu.ni>
2024-10-07 08:36:36 -0600

    Calculadora

---
+++ Ejercicios/Calculadora/Operation/Operationcs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora.Operation
+{
+    internal class Operationcs
+    {
+        public double Sum(double num1, double num2) { return num1 + num2; }
+
+        public double Subst(double num1, double num2) { return num1 - num2; }
+
+        public double Mult(double num1, double num2) { return num1 * num2; }
+
+        public double Divide(double num1, double num2)
+        {
+            try
+            {
+                return num1 / num2;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return 0;
+        }
+
+        public double Pow(double num1, double num2) { return Math.Pow(num1, num2); }
+
+        public double Sqrt(double num) { return Math.Sqrt(num); }
+        public string Out() { return "Adios"; }
+    }
+}
